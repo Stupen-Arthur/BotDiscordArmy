44b3cdfeaa5e58b04be7d54c76fc53afb3ef477f
Arthur Stupen <stupen_artyr@mail.ru>
2023-10-30 18:33:33 +0300

    Added new time

--- index.js
+++ index.js
@@ -44,7 +44,8 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
-          content: `Yo ${interaction.member.user.username}!`,
+          content: `Hello ${interaction.member.user.username}, Here's your army agenda!`,
+		  files: ['img/povestka.jpeg'],
         },
       });
     }
