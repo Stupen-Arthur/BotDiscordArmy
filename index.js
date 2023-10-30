69d30a88a5114a04971457dfde8e1e675c9239fe
Arthur Stupen <stupen_artyr@mail.ru>
2023-10-30 18:19:35 +0300

    Added new time

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const express = require('express');
 const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
 
 const arthurDate = new Date('2024-10-31')
+const baradzinDate = new Date('2024-10-27')
+const kirillDate = new Date('2024-11-02')
+const dzAndVitDate = new Date('2024-11-03')
 
 const app = express();
 // app.use(bodyParser.json());
@@ -50,7 +53,11 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
           content: `Returning back after:
-		  EdWard ${Math.floor((arthurDate - (new Date())) / (1000 * 60 * 60 * 24))}`,
+		  EdWard ${Math.floor((arthurDate - (new Date())) / (1000 * 60 * 60 * 24))}
+		  Eeeee ${Math.floor((kirillDate - (new Date())) / (1000 * 60 * 60 * 24))}
+		  Borodzin ${Math.floor((baradzinDate - (new Date())) / (1000 * 60 * 60 * 24))}
+		  上昇する太陽 ${Math.floor((dzAndVitDate - (new Date())) / (1000 * 60 * 60 * 24))}
+		  𝓔𝓵 𝓜𝓲𝓷𝓲𝓼𝓽𝓻𝓮 ${Math.floor((dzAndVitDate - (new Date())) / (1000 * 60 * 60 * 24))}`,
         },
       });
     }
